2f1bd7564a09e0e28485f5511200ca468c8c5d81
Agustin Tamagno <atamagno@truelogicsoftware.com>
2015-11-09 17:34:47 -0300

    #34 Added grunt module to initialize database with test data.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+﻿module.exports = function (grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        mongoimport: {
+            options: {
+                db: 'yobusco',
+                host: 'localhost',
+                //port: '27017',
+                //username: 'username',
+                //password: 'password',
+                stopOnError: false,
+                collections: [
+                  {
+                      name: 'servicecategories',
+                      file: 'database/servicecategories.json',
+                      drop: true
+                  },
+                  {
+                      name: 'servicesubcategories',
+                      file: 'database/servicesubcategories.json',
+                      drop: true
+                  },
+                  {
+                      name: 'servicesuppliers',
+                      file: 'database/servicesuppliers.json',
+                      drop: true
+                  },
+                  {
+                      name: 'users',
+                      file: 'database/users.json',
+                      drop: true
+                  }
+                ]
+            }
+        }
+    });
+
+    // loadNpmTasks
+    grunt.loadNpmTasks('grunt-mongoimport');
+};
