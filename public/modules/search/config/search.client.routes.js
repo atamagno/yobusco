5a7d66599cb73fd019a0f0be726ec25daf2603b8
Agustin Tamagno <agusto.tamagno@gmail.com>
2015-12-10 14:52:02 -0300

    #22 Initial filters implementation.

--- public/modules/search/config/search.client.routes.js
+++ public/modules/search/config/search.client.routes.js
@@ -14,7 +14,7 @@ angular.module('search').config(
 				templateUrl: 'modules/search/views/results-servicesupplier.client.view.html'
 			}).
 			state('resultsServiceSupplier.list', {
-				url: '/:serviceId/:currentPage/:itemsPerPage',
+				url: '/:serviceId/:currentPage/:itemsPerPage?services&jobAmount&supplierName',
 				templateUrl: 'modules/search/views/results-list.client.view.html'
 			});
 	});
